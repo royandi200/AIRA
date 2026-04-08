58afe99fcc734a5147f4234bac7e29d841958204
Andy <76755871+royandi200@users.noreply.github.com>
2026-04-08 00:46:35 -0500

    fix: add vite-env.d.ts and CSS type declarations

---
+++ src/declarations.d.ts
@@ -0,0 +1,17 @@
+// CSS imports
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+
+// Image imports
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+declare module '*.png' { const content: string; export default content; }
+declare module '*.jpg' { const content: string; export default content; }
+declare module '*.jpeg' { const content: string; export default content; }
+declare module '*.webp' { const content: string; export default content; }
+declare module '*.gif' { const content: string; export default content; }
